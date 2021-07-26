2bf596b581f45443cd04ffc50cafdf1872e1c3a7
jwoo9928 <jwoo9928@gmail.com>
2021-07-26 17:26:30 +0900

    build project

--- src/test/java/com/pie/service/ServiceApplicationTests.java
+++ src/test/java/com/pie/service/ServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.pie.server;
+package com.pie.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class ServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
