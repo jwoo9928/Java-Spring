cf16d2345fa0a0167eef286228eada4561aebf0b
jwoo9928 <jwoo9928@gmail.com>
2021-07-26 16:53:03 +0900

    start project

---
+++ server/src/test/java/com/pie/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pie.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
