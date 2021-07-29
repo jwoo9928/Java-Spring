4b4b147b35ddf146b69f5791591cb00c137588e4
jwoo9928 <jwoo9928@gmail.com>
2021-07-29 09:10:42 +0900

    linked mysql

--- src/main/java/com/pie/service/UserController.java
+++ src/main/java/com/pie/service/UserController.java
@@ -1,6 +1,7 @@
 package com.pie.service;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +13,15 @@ public class UserController {
         System.out.println("output of user Id");
         return "001";
     }
+
+    @GetMapping(path="/userId-bean")
+    public userIdBean userIdBean() {
+        return new userIdBean("001");
+    }
+
+    @GetMapping(path="userInfo/{id}")
+    public userIdBean userInfo(@PathVariable String id) {
+        return new userIdBean(id);
+    }
+
 }
